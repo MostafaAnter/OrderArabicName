da705b98c1cdc0e4bfd6637d22d1808ef16bdc85
Mostafa Anter <ga30000@gmail.com>
2016-06-19 14:57:50 +0200

    early version

--- src/com/mostafa_anter/Main.java
+++ src/com/mostafa_anter/Main.java
@@ -6,14 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// test code by insert some names here
-        TreeSet<Name> mNames = new TreeSet<>();
-        mNames.add(Util.createNameObject("عبدالرحمن"));
-        mNames.add(Util.createNameObject("مصطفى عنتر"));
-        mNames.add(Util.createNameObject("هانى"));
-        mNames.add(Util.createNameObject("عباس"));
-        mNames.add(Util.createNameObject("احمد فرغلي"));
 
-        for (Name name: mNames ) {
+        TreeSet<Name> mNames = new TreeSet<>();
+
+        mNames.add(Util.createNameObject("عبدالرحمن"));
+        mNames.add(Util.createNameObject("مصطفى"));
+        mNames.add(Util.createNameObject("هانى"));
+        mNames.add(Util.createNameObject("احمد"));
+
+        mNames.add(Util.createNameObject("باسم"));
+
+
+
+        for (Name name :
+             mNames) {
             System.out.println(name.getName());
         }
 
